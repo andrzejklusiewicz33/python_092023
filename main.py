42487039d77a07892da8b4ed1b330ef1e356d174
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2023-09-18 11:04:12 +0200

    abc

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 #test
+#  JAVA (fuuuu)
+# class Main{
+#
+#     public static void main(String arg[]){
+#     System.out.println("hello");
+# }
+#
+# }
+#
+# def funkcja():
+#     print('hello')
+#
+# print('hello')
+# print("hello")
+# print("Mc'Donalds")
+# #imie='Andrzej'
+# #PEP8
+# first_name='Andrzej'
+# print(first_name,type(first_name))
+# #String firstName='Andrzej'
+# first_name=123
+# print(first_name,type(first_name))
+# first_name=1.23
+# print(first_name,type(first_name))
+# first_name=input('podaj imię:\n')
+# print(first_name)
+#x=input('podaj wartość x:\n')
+#print(x,type(x))
+#print(x/2)
+# fruit='Banana'
+# color='blue'
+# print("Twój ulubiony owoc to : ",fruit)
+# print("Twój ulubiony owoc to: "+fruit)
+# print("Twój ulubiony owoc to {}, a kolor {}".format(fruit,color,'UWAGA'))
+#
+# x=123
+# y=345
+# print('x='+str(x) )
+# print('x={}'.format(x))
+# print(f'x={x} y={y}')
+
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+#   wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
